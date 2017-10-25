72092e6e8dd976b219c9b17f5042d0dd4204e6f2
EdwardWilliamStephenStourton <33100782+EdwardWilliamStephenStourton@users.noreply.github.com>
2017-10-25 21:59:29 +0200

    Update god.js

--- god.js
+++ god.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  client.user.setPresence({ game: { name: 'Chat with God', type: 0 } });
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-	if(,essage.content==='ping'){
-		message.reply('pong');
-	}
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 client.login(process.env.BOT_TOKEN);
