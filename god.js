f6e2d5d83e66330307f71651661ab8c1ea53c873
EdwardWilliamStephenStourton <33100782+EdwardWilliamStephenStourton@users.noreply.github.com>
2017-10-25 21:54:40 +0200

    Update god.js

--- god.js
+++ god.js
@@ -6,12 +6,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  let channel = bot.channels.get('372811432636710913');
+	let channel = bot.channels.get('372811432636710913');
 	var keyword = 'God';
-  var user_command=message.content;
-  if(user_command.indexOf(keyword)!==-1){
-    channel.send("I'm in business bitches!");
-  }
+	var user_command = message.content;
+	if(user_command.indexOf(keyword)!==-1){
+		channel.send('I am in business bitches!');
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
