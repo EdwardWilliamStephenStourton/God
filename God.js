2c5545fd5512f0d7dbca0559d929fe6527bf5c0d
EdwardWilliamStephenStourton <33100782+EdwardWilliamStephenStourton@users.noreply.github.com>
2017-10-25 20:51:54 +0200

    Update God.js

--- God.js
+++ God.js
@@ -1,13 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-function bot_response(var param){
-  if(param.indexOf(Hello)!==-1||param.indexOf(Hi)!==-1||param.indexOf(Morning)!==-1||param.indexOf(Afternoon)!==-1||param.indexOf(Evening)!==-1||param.indexOf(Hey)!==-1){
-    channel.send('Welcome, oh Master!');
-  }
-  return 0;
-}
-
 bot.on('ready', () => {
   bot.user.setPresence({ game: { name: 'Chat with God ^^', type: 0 } });
 });
@@ -17,7 +10,6 @@ bot.on('message', message => {
   var keyword = 'God';
   var user_command = message.content;
   if(user_command.indexOf(keyword)!==-1){
-    //bot_response(user_command);
     channel.send('I am working fam!');
   }
 });
