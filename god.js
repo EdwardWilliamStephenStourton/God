a70b07ba8fb8eb45277e851b87bf2b5a01c9664e
EdwardWilliamStephenStourton <33100782+EdwardWilliamStephenStourton@users.noreply.github.com>
2017-10-25 21:47:06 +0200

    Update god.js

--- god.js
+++ god.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  client.user.setPresence({ game: { name: 'Chat with God', type: 0 } });
 });
 
 client.on('message', message => {
